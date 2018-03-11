3b7af9faf839a48615804abdddd985d562aacd85
Bobrovskii V <bobrovskii@moedelo.org>
2018-03-11 12:48:40 +0400

    remove

---
+++ SimpleExample/Models/TestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CorsExample1.Models
+{
+    public class TestModel
+    {
+        public string AppName { get; set; } = "Simple Example";
+        public int TestId { get; set; }
+        public string TestMessage { get; set; }
+    }
+}
