dbcc2195c252283099f23d92e65fdf27be45251f
Bobrovskii V <bobrovskii@moedelo.org>
2018-03-11 12:13:25 +0400

    add project

--- WebAPI2Cors/Models/TestModel.cs
+++ WebAPI2Cors/Models/TestModel.cs
@@ -7,6 +7,7 @@ namespace WebAPI2Cors.Models
 {
     public class TestModel
     {
+        public string AppName { get; set; } = "Custom Cors Attribute";
         public int TestId { get; set; }
         public string TestMessage { get; set; }
     }
